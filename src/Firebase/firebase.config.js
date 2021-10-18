660287ac37542cb0fcabc995ce6cfbe6b802e219
Nure-Alam-sidddique <74979607+Nure-Alam-sidddique@users.noreply.github.com>
2021-10-18 20:03:47 +0600

    install firebase,router,tailwindcss and firebase config

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDqUpMHrmcIzFT8WmZCS73HCpSwzzHC-s8",
+  authDomain: "naz-pharma.firebaseapp.com",
+  projectId: "naz-pharma",
+  storageBucket: "naz-pharma.appspot.com",
+  messagingSenderId: "782802924620",
+  appId: "1:782802924620:web:b8032bdd59b4c8c3b3f0e9",
+};
+export default firebaseConfig;
